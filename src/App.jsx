888ecb3a12556578a1ab38e5e7614629ede3eb2f
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-08-20 23:18:38 +0430

    i use the if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
 
-import Footer from './component/Footer';
-import Header from './component/Header';
-import Contact from './component/Contact';
-import Hero from './component/Hero';
+// import Footer from './component/Footer';
+// import Header from './component/Header';
+// import Contact from './component/Contact';
+// import Hero from './component/Hero';
 
 const App = () => {
+
+  let marks = 80;
   return (
     <div>
       {/* <h1>Welcome to My React App</h1>
       <input type="text" placeholder="enter your name" />
       <button>submit</button> */}
-      <h2 style={
+
+
+      {/* <h2 style={
         {
         color: 'red',
         }
       }>hello react</h2>
 
-      <h2>{new Date().getTime()}</h2>
+      <h2>{new Date().getTime()}</h2> */}
+
+      {/* if and else use */}
+
+      {
+        marks >80 ? <h1>Your result  is greater then {marks}</h1>
+        : <h1>your result is less then  {marks}</h1>
+      }
+
 
       {/* <Header />
       <Footer />
