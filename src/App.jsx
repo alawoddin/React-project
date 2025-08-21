0a04f13665fa309263d5ebcb4e4ddd8ec8b62252
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-08-21 21:45:09 +0430

    loop is add or work

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,19 @@
 
 const App = () => {
 
-  let marks = 80;
+  const city = ['farah' , 'kabul' , 'harat', 'mazar', 'jalalabad', 'kandahar'];
   return (
+ 
     <div>
-      {/* <h1>Welcome to My React App</h1>
-      <input type="text" placeholder="enter your name" />
-      <button>submit</button> */}
-
-
-      {/* <h2 style={
-        {
-        color: 'red',
-        }
-      }>hello react</h2>
-
-      <h2>{new Date().getTime()}</h2> */}
-
-      {/* if and else use */}
-
-      {
-        marks >80 ? <h1>Your result  is greater then {marks}</h1>
-        : <h1>your result is less then  {marks}</h1>
-      }
-
-
-      {/* <Header />
-      <Footer />
-      <Contact />
-      <Hero /> */}
+      <ul>
+        <li>
+          {
+            city.map((item,i)=>{
+              return <li key={i.toString}>{item}</li>
+            })
+          }
+        </li>
+      </ul>
 
     </div>
   );
