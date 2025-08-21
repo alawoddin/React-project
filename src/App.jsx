4faac2d206fedab4260725b1daa672f583a22f40
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-08-21 22:00:15 +0430

    if and else

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,51 @@
 // import Contact from './component/Contact';
 // import Hero from './component/Hero';
 
+const LoginStatusBtn= (status) => {
+  if(status) {
+    return <button>Logout</button>
+  }else {
+    return <button>Login</button>
+  }
+}
+
 const App = () => {
-
-  const city = ['farah' , 'kabul' , 'harat', 'mazar', 'jalalabad', 'kandahar'];
   return (
- 
     <div>
-      <ul>
-        <li>
-          {
-            city.map((item,i)=>{
-              return <li key={i.toString}>{item}</li>
-            })
-          }
-        </li>
-      </ul>
-
+      <h1>login status </h1>
+      {LoginStatusBtn(false)}
     </div>
-  );
-};
+  )
+}
 
 export default App;
+
+// const App = () => {
+
+//   // let status = true;
+//   let status = false;
+
+//   if(status == true) {
+
+//       return (
+ 
+//     <div>
+//       <h1>Login Status</h1>
+//       <button>Logout</button>
+//     </div>
+//   );
+// }
+//   else {
+//     return (
+//       <div>
+//         <h1>login Status</h1>
+//         <button>login</button>
+//       </div>
+//     )
+//   }
+
+//   }
+
+
+// export default App;
+
